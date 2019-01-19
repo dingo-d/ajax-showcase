4b8d8816a6677e8e617030cc532442aba9916ae6
Denis Žoljom <denis.zoljom@gmail.com>
2019-01-19 12:19:23 +0100

    Add init file for ajax in admin plugin

---
+++ ajax-admin-pages/ajax-admin-pages.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin main file starting point
+ *
+ * @since             1.0.0
+ * @package           Ajax_In_Admin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       AJAX in WordPress Admin
+ * Plugin URI:        https://github.com/dingo-d/ajax-showcase/ajax-admin-pages
+ * Description:       The example of a simple plugin for adding ajax in admin pages
+ * Version:           1.0.0
+ * Author:            Denis Žoljom <denis.zoljom@gmail.com>
+ * Author URI:        https://madebydenis.com
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
+ * Text Domain:       ajax-in-admin
+ * Requires PHP:      5.6
+ */
